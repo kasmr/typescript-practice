49a1e46f541a1d17ca5b6148e92ac816055dc5e4
kasmr <alikkras@gmail.com>
2020-05-14 17:12:30 +0300

    types

---
+++ types.ts
@@ -0,0 +1,49 @@
+const isFetching: boolean = true;
+const isLoading: boolean = false;
+
+const int: number = 42;
+const float: number = 4.2;
+const num: number = 3e10;
+
+const message: string = 'Typescript';
+
+const numberArray: number[] = [1, 2, 3, 5, 8, 13];
+const numberArray2: Array<number> = [1, 2, 3, 5, 8, 13];
+
+const words: string[] = ['WhatsUp', 'Typescript'];
+
+// Tuple/Кортеж
+const contact: [string, number] = ['Alik', 123455];
+
+// Any
+let variable: any = 42;
+
+variable = 'New string';
+variable = [];
+
+// Function
+function sayMyName(name: string): void {
+  console.log(name);
+}
+sayMyName('Heisenberg');
+
+// Never
+function throwError(message: string): never {
+  throw new Error(message);
+}
+
+function infinite(): never {
+  while (true) {}
+}
+
+// Type
+type Login = string;
+
+const login: Login = 'admin';
+// const login2: Login = 2
+
+type ID = string | number;
+const id1: ID = 1234;
+const id2: ID = '153';
+
+type SomeType = string | null | undefined;
