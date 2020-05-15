a5664d4f743773d042dea0e0afd7999cb25e8800
kasmr <alikkras@gmail.com>
2020-05-15 17:34:47 +0300

    functions and enum

---
+++ enum.ts
@@ -0,0 +1,16 @@
+enum Membership {
+  Simple,
+  Standard,
+  Premium,
+}
+
+const membership = Membership.Standard;
+const membershipReverse = Membership[2];
+
+enum SocialMedia {
+  VK = 'VK',
+  FACEBOOK = 'FACEBOOK',
+  INSTA = 'INSTA',
+}
+
+const social = SocialMedia.INSTA;
